d0b01409d3e18ebec5d43de488113ad0c194dcb1
Simone <simonelega.sl@gmail.com>
2019-12-19 17:25:40 +0100

    Faccio le operazioni e stampo i prezzi con eventuali sconti

--- js/trenoform.js
+++ js/trenoform.js
@@ -1,21 +1,35 @@
 
 
-
-// var prezzoBase = 0.21;
-// var prezzoTratta= prezzoBase * chilometri;
-// var euro = " €";
-
 var genera= document.getElementById("genera");
 
 genera.addEventListener('click',
   function(){
     // prendo il nome del passeggero e lo inserisco nel tag specifico
-  var nome= document.getElementById('nome').value;
+  var nome = document.getElementById('nome').value;
   var nomePasseggero = document.getElementById('nome_passeggero');
   nomePasseggero.innerHTML = nome;
 
-  var chilometri
+  // prendo i chilometri e la fascia d'età
+  var km = document.getElementById('km').value;
+  var fascia = document.getElementById('fascia').value;
+
+  var prezzoBase = 0.21;
+  var prezzoTratta= prezzoBase * km;
+  var euro = " €";
+  var num = 5.304;
+  var n= num.toFixed(2);
+  console.log(n);
 
 
+// va applicato uno sconto del 20% per i minorenni e del 40% per gli over 65.
+  if (fascia == 'minorenne') {
+    prezzoTratta -= prezzoTratta * (20 / 100);
+    document.getElementById('costo_biglietto').innerHTML = "Il tuo biglietto costa " + prezzoTratta.toFixed(2) + euro;
+  } else if (fascia == 'silvercard') {
+    prezzoTratta -= prezzoTratta * (40 / 100);
+    document.getElementById('costo_biglietto').innerHTML = "Il tuo biglietto costa " + prezzoTratta.toFixed(2) + euro;
+  }
+
+  document.getElementById('costo_biglietto').innerHTML = "Il tuo biglietto costa " + prezzoTratta + euro;
   }
 );
