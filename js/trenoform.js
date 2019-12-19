3e5148ac80e1c74bab017752871e064f5f20ef50
Simone <simonelega.sl@gmail.com>
2019-12-19 16:51:31 +0100

    prendo il nome del passeggero e lo inserisco nel tag specifico

--- js/trenoform.js
+++ js/trenoform.js
@@ -1,1 +1,21 @@
 
+
+
+// var prezzoBase = 0.21;
+// var prezzoTratta= prezzoBase * chilometri;
+// var euro = " €";
+
+var genera= document.getElementById("genera");
+
+genera.addEventListener('click',
+  function(){
+    // prendo il nome del passeggero e lo inserisco nel tag specifico
+  var nome= document.getElementById('nome').value;
+  var nomePasseggero = document.getElementById('nome_passeggero');
+  nomePasseggero.innerHTML = nome;
+
+  var chilometri
+
+
+  }
+);
